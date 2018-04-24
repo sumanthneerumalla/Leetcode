f94bf662ede0c133da4093c8b77625db0a2da59c
Sumanth Neerumalla <sumanth42gold@gmail.com>
2018-04-24 17:03:43 -0400

    had to get help by looking at an existing solution. We will get better.

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -13,29 +13,34 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        curr1 = l1
-        curr2 = l2
-        next1 = l1.next
-        next2 = l2.next
-        sumA = []
+
+        start = mylist = ListNode(0)
+
+        next1 = l1
+        next2 = l2
         carry = 0
-        while(next1 != None and next2 != None):
-            sum = curr1 + curr2 + carry
-            if sum >9:
-                sumA.insert(0,sum %10)
+
+        while (next1 or next2 or carry):
+
+            val1 = val2 = 0
+            if next1:
+                val1 = next1.val
+                next1 = next1.next
+            if next2:
+                val2 = next2.val
+                next2 = next2.next
+
+            tot = val1 + val2 + carry
+
+            if tot > 9:
+                tot = tot % 10
                 carry = 1
             else:
-                sumA.insert(0,sum)
                 carry = 0
-            next1 = curr1.next
-            next2 = curr2.next
-            #at the end of the while loop one of the lists has hit an end, so we add the remaining digits in the other list
-            if next2 == None:
-                    while(next1 != None):
-                        sumA.insert(0, next1)
-                        next1 = curr1.next
-            else:#either the next1 or the next 2 is ended
-                    while(next2 != None):
-                        sumA.insert(0, next2)
-                        next2 = curr2.next
+
+            mylist.next = ListNode(tot)
+            mylist = mylist.next
+
+        return start.next
+
 
